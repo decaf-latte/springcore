8d6e966ea6a7a22ea618b5946c295d5996c5fc5e
decaf-latte <hyejin.jeon940120@gmail.com>
2022-08-23 11:37:33 +0900

    2-15 카카오 사용자 정보 가져오기 (토큰)

---
+++ src/main/java/com/sparta/springcore/Dto/KakaoUserInfoDto.java
@@ -0,0 +1,13 @@
+package com.sparta.springcore.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+  private Long id;
+  private String nickname;
+  private String email;
+
+
+}
+
